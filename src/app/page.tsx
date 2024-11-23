c7d230dfc70c38d2342a7fa4daaddea1cae919b4
“[javed-ali92] <[javed.ali.1121974@gmail.com]>
2024-11-23 23:45:04 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Advance from '@/components/Advance';
 import Compulsory from '@/components/Compulsory';
 import Hero from '@/components/Hero';
 import Text from "@/components/Text";
-import Image from "next/image";
 
 
 
